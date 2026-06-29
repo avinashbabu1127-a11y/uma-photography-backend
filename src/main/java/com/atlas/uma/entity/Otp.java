5918628f913109b6c9515a8781f67565105f22ad
avinashbabu1127-a11y <avinashbabu.1127@gmail.com>
2026-06-29 16:07:50 +0530

    Added otp entity and repository

---
+++ src/main/java/com/atlas/uma/entity/Otp.java
@@ -0,0 +1,32 @@
+package com.atlas.uma.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "otp")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Otp {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String otp;
+
+    @Column(nullable = false)
+    private LocalDateTime expiryTime;
+
+    @Column(nullable = false)
+    private Boolean verified;
+}
