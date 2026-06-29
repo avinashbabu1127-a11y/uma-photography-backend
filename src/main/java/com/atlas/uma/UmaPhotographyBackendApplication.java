58775e3609980649f2b0162f3fe379d742ce9c94
avinashbabu1127-a11y <avinashbabu.1127@gmail.com>
2026-06-29 10:21:45 +0530

    implement the spring installizer

---
+++ src/main/java/com/atlas/uma/UmaPhotographyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.atlas.uma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmaPhotographyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmaPhotographyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atlas/uma/UmaPhotographyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atlas.uma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmaPhotographyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
